346b961ebf402ee5b09e7ab9b5172e62ff675314
Pha <anhphamhoang04@gmail.com>
2025-04-17 05:37:07 +0700

    add test

--- source/test/testmysqldata.py
+++ source/test/testmysqldata.py
@@ -7,12 +7,12 @@ class Collector:
     def __init__(self):
         self.engine = create_engine("mysql+pymysql://root:mps2024@localhost:3306/iot_data")
 
-    def collect(self, downSampleRate=441):
+    def collect(self):
 
         def callback(indata, frames, time, status):
             if status:
                 print(status)
-            dataFrame = pd.DataFrame({"Amplitude": indata[:, 0]})
+            dataFrame = pd.DataFrame({"Amplitude": indata[::100, 0]}) # downsample to 441
             dataFrame.to_sql("thuthap", con=self.engine, if_exists="append", index=False)
 
         with sd.InputStream(samplerate=44100, channels=1, callback=callback):
