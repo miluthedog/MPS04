27a89a17c35090b3a80cce7a1ba8525556403f72
Pha <anhphamhoang04@gmail.com>
2025-04-18 23:52:31 +0700

    test

--- source/test/0testplot1.py
+++ source/test/0testplot1.py
@@ -1,20 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
 
 class Plot:
     def __init__(self):
         pass
 
-    def plotdata(self, filename):
-        data = pd.read_csv(f"source/data/{filename}.csv")
-        plt.plot(data)
-
+    def plotdata(self, filename, samplerate=44100):
+        data = pd.read_csv(f"source/data/{filename}.csv").iloc[:,0]
+        time = np.arange(len(data)) / samplerate
+        plt.plot(time, data)
         plt.title("Test signal")
-        plt.xlabel("ID")
-        plt.ylabel(data.columns[0])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude")
         plt.ylim(-0.5, 0.5)
         plt.show()
 
-
-Plot().plotdata("bbbb1")
+Plot().plotdata("E1bwbw1", samplerate=44100)
--- source/test/3testrms.py
+++ source/test/3testrms.py
@@ -1,13 +1,42 @@
-from scipy.signal import find_peaks, welch, medfilt
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Processor:
-    def __init__(self, filename):
+    def __init__(self, filename, fs=44100):
         self.filename = f"source/data/{filename}.csv"
+        self.fs = fs  # Sampling rate in Hz
 
-    def process(self):
-        data = pd.read_csv(self.filename)
-        signal = data["Amplitude"].values
+    def process(self, window_ms=1):
+        signal = pd.read_csv(self.filename)["Amplitude"].values
+        total_samples = len(signal)
 
-        
+        # Convert window size in ms to samples
+        window_size = int(self.fs * window_ms / 1000)
+
+        # Compute RMS using non-overlapping windows
+        rms_values = [
+            np.sqrt(np.mean(signal[i:i+window_size]**2))
+            for i in range(0, total_samples - window_size, window_size)
+        ]
+
+        # Time axes
+        t_signal = np.arange(total_samples) / self.fs
+        t_rms = np.arange(len(rms_values)) * (window_size / self.fs)
+
+        # Plot both
+        plt.figure(figsize=(10, 5))
+        plt.plot(t_signal, signal, label="Original Signal", alpha=0.5)
+        plt.plot(t_rms, rms_values, label=f"RMS ({window_ms}ms)", color="red", linewidth=2)
+        plt.ylim(-0.5, 0.5)
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude / RMS")
+        plt.title("Signal and RMS Over Time")
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+        return rms_values
+
+p = Processor("E1bwbw1")
+p.process(window_ms=1)
